76f8ea20f4a24ed246947db5195b8f78fcf6c158
voidanix <voidanix@420blaze.it>
2019-07-09 14:43:35 +0200

    bullhead: remove dexpreopt config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,15 +113,10 @@ HAVE_ADRENO_SOURCE:= false
 
 OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 
-# Enable dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
-	  WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+# Dexpreopt
+WITH_DEXPREOPT_DEBUG_INFO := false
+USE_DEX2OAT_DEBUG := false
+DONT_DEXPREOPT_PREBUILTS := true
 
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
