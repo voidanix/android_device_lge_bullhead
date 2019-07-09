221fb08d118a663c2c155c1f97f4bfd5a3e228a4
voidanix <voidanix@420blaze.it>
2019-07-09 20:09:45 +0000

    bullhead: use binderized power HAL

--- device.mk
+++ device.mk
@@ -514,8 +514,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    power.bullhead \
-    android.hardware.power@1.0-impl
+    android.hardware.power@1.1-service-qti
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
