d58214e7cf6f7ef90e13a99b9c3d926829d47481
voidanix <voidanix@420blaze.it>
2019-08-20 00:18:32 +0200

    Revert "bullhead: use binderized power HAL"
    This reverts commit 221fb08d118a663c2c155c1f97f4bfd5a3e228a4.

--- device.mk
+++ device.mk
@@ -514,7 +514,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    power.bullhead \
+    android.hardware.power@1.0-impl
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
