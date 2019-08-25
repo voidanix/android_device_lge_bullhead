faf586b153a1da1a55c52f6432dc227532b94f3b
voidanix <voidanix@420blaze.it>
2019-08-26 01:49:25 +0200

    bullhead: use local headers

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -63,7 +63,7 @@ LOCAL_C_INCLUDES += \
         frameworks/native/include/media/hardware \
         device/lge/bullhead/camera/QCamera2/HAL
 
-TARGET_HEADER_LIBRARIES := generated_kernel_headers
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 #LOCAL_STATIC_LIBRARIES := libqcamera2_util
 LOCAL_C_INCLUDES += \
