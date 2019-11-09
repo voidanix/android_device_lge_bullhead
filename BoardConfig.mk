7b93ab1e3c96265130ff50322a09c2ebc69720e8
voidanix <voidanix@420blaze.it>
2019-11-09 12:30:17 +0100

    Fix kernel toolchain and another neverallow

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,10 +35,11 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
 TARGET_KERNEL_CONFIG := lineageos_bullhead_defconfig
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
