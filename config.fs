3143a7945c6ac20b00136a5bde24de59a5b1415d
voidanix <voidanix@420blaze.it>
2019-11-18 19:55:05 +0100

    Let config.fs handle /persist
    We are probably going to move this somewhere in /mnt/persist or
    something like the Pixels do, but as of now, keep it like this

--- config.fs
+++ config.fs
@@ -3,8 +3,15 @@ mode: 0700
 user: AID_RADIO
 group: AID_SHELL
 caps: BLOCK_SUSPEND
+
 [vendor/bin/mm-qcamera-daemon]
 mode: 0700
 user: AID_CAMERA
 group: AID_SHELL
 caps: SYS_NICE
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
