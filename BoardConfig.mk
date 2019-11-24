26e5442265e34cebe84cec36c03bd192ff2a3bd3
voidanix <voidanix@420blaze.it>
2019-11-24 12:31:12 +0100

    /persist and /firmware are deprecated mountpoints
    The Pixels have marked them as deprecated for a long time, they even
    filed a bug for them so they could be removed in the future
    
    Let's keep them around like bonito since it doesn't get past the
    bootloader without them (apparently)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,9 +33,8 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53.a57
 
 TARGET_NO_BOOTLOADER := true
 
-#BOARD_ROOT_EXTRA_FOLDERS := persist #maybe replace this with /mnt since it's /mnt/vendor/persist now, check the symlinks as well
-#BOARD_ROOT_EXTRA_SYMLINKS := /vendor/firmware_mnt:/firmware
-#BOARD_ROOT_EXTRA_SYMLINKS += /mnt/vendor/persist:/persist
+BOARD_ROOT_EXTRA_SYMLINKS := /mnt/vendor/persist:/persist
+BOARD_ROOT_EXTRA_SYMLINKS += /vendor/firmware_mnt:/firmware
 
 # Inline kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
